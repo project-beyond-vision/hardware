93efb58273db4edbc7d91e6296c0540d0ddfc108
Ramana <ramanalfc@yahoo.com.sg>
2022-09-29 15:43:13 +0800

    [Avoidance Sensor] Initial Commit

---
+++ hardware/Avoidance_Sensor.ino
@@ -0,0 +1,25 @@
+#define LED D4 // Use the onboard Uno LED
+#define isObstaclePin D3  // This is our input pin
+int isObstacle = HIGH;  // HIGH MEANS NO OBSTACLE
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(isObstaclePin, INPUT);
+  Serial.begin(9600);  
+}
+
+void loop() {
+  isObstacle = digitalRead(isObstaclePin);
+  if (isObstacle == LOW)
+  {
+    Serial.println("OBSTACLE!!, OBSTACLE!!");
+    digitalWrite(LED, HIGH);
+  }
+  else
+  {
+    Serial.println("clear");
+    digitalWrite(LED, LOW);
+  }
+  Serial.println(isObstacle);
+  delay(200);
+}
