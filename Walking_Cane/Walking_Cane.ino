be0dbdb73d0efdffdfe00d77ce9b304b68e380d8
Ramana <ramanalfc@yahoo.com.sg>
2022-10-04 13:09:48 +0800

    [Walking Cane] Init Flame Sensor

--- Walking_Cane/Walking_Cane.ino
+++ Walking_Cane/Walking_Cane.ino
@@ -1,13 +1,18 @@
 #define SWITCH D4
 #define BUZZER D3
 
+#define THRESHOLD 200
+
 volatile byte is_pressed = LOW;
 byte button_state = LOW;
 byte sound_state = LOW;
 unsigned short cnt = 0;
 unsigned long last_sound_time = 0;
 unsigned long last_pressed_time = 0;
-#define THRESHOLD 200
+
+// lowest and highest flame sensor readings:
+const int sensorMin = 0;     // sensor minimum
+const int sensorMax = 1024;  // sensor maximum
 
 IRAM_ATTR void toggle() {
   is_pressed = HIGH;
@@ -25,6 +30,27 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // read the sensor on analog A0:
+  int sensorReading = analogRead(A0);
+  // map the sensor range (four options):
+  // ex: 'long int map(long int, long int, long int, long int, long int)'
+  int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
+
+  // range value:
+  switch (range) {
+  case 0:    // A fire closer than 1.5 feet away.
+    Serial.println("** Close Fire **");
+    // play immediate danger sound
+    break;
+  case 1:    // A fire between 1-3 feet away.
+    Serial.println("** Distant Fire **");
+    // play intermittent sound
+    break;
+  case 2:    // No fire detected.
+    Serial.println("No Fire");
+    // do nothing
+    break;
+  }
   
   if(is_pressed == HIGH) {
     is_pressed = LOW;
